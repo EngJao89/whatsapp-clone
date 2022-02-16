9dffc27a9f1a623be3da9080938c80abec05f1ba
EngJao89 <eng_joao@outlook.com>
2022-02-15 23:29:43 -0300

    Closes 03:"Implementa caixa de diretório."

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
-const { app, Menu, Tray } = require('electron');
+const { app, Menu, Tray, dialog } = require('electron');
 
 app.dock.hide();
 
@@ -7,7 +7,10 @@ app.on('ready', () => {
   const tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
 
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio', checked: true }
+    { label: 'Item1', type: 'radio', checked: true, click: () => {
+      const path = dialog.showOpenDialog({ properties: ['openDirectory'] });
+        console.log(path);
+    } }
   ]);
 
   tray.setToolTip('This is my application');
