68d434dde763faf12f561cd0f1d57e1dc0157f5a
EngJao89 <eng_joao@outlook.com>
2022-02-18 01:26:15 -0300

    Closes 05:"Melhora seleção de pasta."

--- main.js
+++ main.js
@@ -17,20 +17,30 @@ app.on('ready', () => {
   const storedProjects = store.get('projects');
   const projects = storedProjects ? JSON.parse(storedProjects) : [];
 
-  const items = projects.map((project) => {
-    return { label: project.name, click: () => spawn.sync('code', [project.path])}
-  });
+  const items = projects.map(project => ({ 
+    label: project.name, 
+    click: () => { 
+      spawn.sync('code', [project.path]);
+    }
+  }));
 
   console.log(projects);
 
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio', checked: true, click: () => {
-      const path = dialog.showOpenDialog({ properties: ['openDirectory'] });
+    ...items,
+    { 
+      type: 'separator',
+    },
+    { 
+      label: 'Adicionar novo projeto...', 
+      click: () => {
+        const path = dialog.showOpenDialog({ properties: ['openDirectory'] });
         store.set('projects', JSON.stringify([...projects, {
           path,
           name: baseName(path),
         }]));
-    } }
+      }, 
+    },
   ]);
 
   tray.setToolTip('This is my application');
